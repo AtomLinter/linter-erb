0a4f87dfb0ced7a8174332cff8a093b923fdc8c2
Landon Abney <Landonabney@gmail.com>
2017-09-04 16:08:41 -0700

    Asyncify the specs
    Bring in `jasmine-fix` to allow the usage of `async`/`await` in the
    specs, and refactor them to take advantage of this.

--- spec/linter-erb-spec.js
+++ spec/linter-erb-spec.js
@@ -1,5 +1,7 @@
 'use babel';
 
+// eslint-disable-next-line no-unused-vars
+import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
 import * as path from 'path';
 import linter from '../lib/index';
 
@@ -7,75 +9,53 @@ import linter from '../lib/index';
 describe('The ERB provider for Linter', () => {
   const { lint } = linter.provideLinter();
 
-  beforeEach(() => {
+  beforeEach(async () => {
     atom.workspace.destroyActivePaneItem();
     const activationPromise = atom.packages.activatePackage('linter-erb');
 
-    waitsForPromise(() =>
-      atom.packages.activatePackage('language-ruby'));
+    await atom.packages.activatePackage('language-ruby');
 
-    waitsForPromise(() =>
-      atom.workspace.open(path.join(__dirname, 'fixtures', 'good.html.erb')));
+    await atom.workspace.open(path.join(__dirname, 'fixtures', 'good.html.erb'));
 
     atom.packages.triggerDeferredActivationHooks();
-    waitsForPromise(() =>
-      activationPromise);
+    await activationPromise;
   });
 
-  it('should be in the packages list', () =>
-    expect(atom.packages.isPackageLoaded('linter-erb')).toBe(true));
+  it('should be in the packages list', () => {
+    expect(atom.packages.isPackageLoaded('linter-erb')).toBe(true);
+  });
 
-  it('should be an active package', () =>
-    expect(atom.packages.isPackageActive('linter-erb')).toBe(true));
+  it('should be an active package', () => {
+    expect(atom.packages.isPackageActive('linter-erb')).toBe(true);
+  });
 
-  describe('checks a file with issues and', () => {
-    let editor = null;
+  it('returns the expected messages for a file with issues', async () => {
     const badFile = path.join(__dirname, 'fixtures', 'bad.html.erb');
-    beforeEach(() => {
-      waitsForPromise(() =>
-        atom.workspace.open(badFile).then((openEditor) => {
-          editor = openEditor;
-        }));
-    });
+    const messageText = 'unexpected keyword_in, expecting end-of-input';
+    const editor = await atom.workspace.open(badFile);
+    const messages = await lint(editor);
 
-    it('finds at least one message', () => {
-      waitsForPromise(() =>
-        lint(editor).then((messages) => {
-          expect(messages.length).toBeGreaterThan(0);
-        }));
-    });
-
-    it('verifies the first message', () => {
-      waitsForPromise(() => {
-        const messageText = 'unexpected keyword_in, expecting end-of-input';
-        return lint(editor).then((messages) => {
-          expect(messages[0].type).toBe('Error');
-          expect(messages[0].html).not.toBeDefined();
-          expect(messages[0].text).toBe(messageText);
-          expect(messages[0].filePath).toBe(badFile);
-          expect(messages[0].range).toEqual([[0, 0], [0, 32]]);
-        });
-      });
-    });
+    expect(messages.length).toBe(1);
+    expect(messages[0].type).toBe('Error');
+    expect(messages[0].html).not.toBeDefined();
+    expect(messages[0].text).toBe(messageText);
+    expect(messages[0].filePath).toBe(badFile);
+    expect(messages[0].range).toEqual([[0, 0], [0, 32]]);
   });
 
-  it('finds nothing wrong with a file with rails type blocks', () => {
-    waitsForPromise(() => {
-      const blocksFile = path.join(__dirname, 'fixtures', 'rails_blocks.html.erb');
-      return atom.workspace.open(blocksFile).then(editor =>
-        lint(editor).then((messages) => {
-          expect(messages.length).toBe(0);
-        }));
-    });
+  it('finds nothing wrong with a file with rails type blocks', async () => {
+    const blocksFile = path.join(__dirname, 'fixtures', 'rails_blocks.html.erb');
+    const editor = await atom.workspace.open(blocksFile);
+    const messages = await lint(editor);
+
+    expect(messages.length).toBe(0);
   });
 
-  it('finds nothing wrong with a valid file', () => {
-    waitsForPromise(() => {
-      const goodFile = path.join(__dirname, 'fixtures', 'good.erb');
-      return atom.workspace.open(goodFile).then(editor =>
-        lint(editor).then((messages) => {
-          expect(messages.length).toBe(0);
-        }));
-    });
+  it('finds nothing wrong with a valid file', async () => {
+    const goodFile = path.join(__dirname, 'fixtures', 'good.erb');
+    const editor = await atom.workspace.open(goodFile);
+    const messages = await lint(editor);
+
+    expect(messages.length).toBe(0);
   });
 });
